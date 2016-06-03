3df6b4435834d3f5377198ae0ebd065ceeccfd83
Bucky <buckyroberts@gmail.com>
2016-06-02 21:25:49 -0400

    Initial commit

---
+++ 01_inheritance/README.md
@@ -0,0 +1,4 @@
+## Inheritance
+
+- when you have a child class that gets basic behavior from parent class
+- used to avoid rewriting common functions in every class that shares similar behavior
---
+++ 01_inheritance/main.py
@@ -0,0 +1,33 @@
+class Enemy:
+
+    def move_left(self):
+        print('Moving left...')
+
+    def move_right(self):
+        print('Moving left...')
+
+
+class Ninja(Enemy):
+
+    def karate_chop(self):
+        print('Karate chop!')
+
+
+class Zombie(Enemy):
+
+    def bite(self):
+        print('I am biting you!')
+
+
+enemy = Enemy()
+enemy.move_left()
+
+# Ninja also includes all functions from parent class (Enemy)
+ninja = Ninja()
+ninja.move_left()
+ninja.karate_chop()
+
+# Zombie is called the (child class), inherits from Enemy (parent class)
+zombie = Zombie()
+zombie.move_right()
+zombie.bite()
