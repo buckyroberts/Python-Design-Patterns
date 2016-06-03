801b913aa5fd1985ef14d53df0ca37e924819c1f
buckyroberts <buckyroberts@gmail.com>
2016-06-03 01:19:28 -0500

    Added Singleton example

---
+++ 04_singleton/README.md
@@ -0,0 +1,3 @@
+## Singleton
+
+The singleton pattern is used to ensure one and only one object from a class gets created.
---
+++ 04_singleton/main.py
@@ -0,0 +1,25 @@
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if not Singleton.__instance:
+            print('Instance does not exist...')
+        else:
+            print('Instance exists:', self.get_instance())
+
+    @classmethod
+    def get_instance(cls):
+        if not cls.__instance:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+a = Singleton()
+
+# Once a singleton is created, all instances of this class will use the same object
+print('Creating instance', Singleton.get_instance())
+
+# Since singleton already created, the existing object is returned
+b = Singleton()
+
+# Even though we now have 3 different instances of this class, they all reference the same object
+c = Singleton()
