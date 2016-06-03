342bf61e824b313872aa211cdfcf1d9a393a8f1a
buckyroberts <buckyroberts@gmail.com>
2016-06-03 02:36:16 -0500

    Observer pattern

---
+++ 06_observer/README.md
@@ -0,0 +1,4 @@
+## Observer
+
+Used when you need many objects to update when another object changes
+
---
+++ 06_observer/main.py
@@ -0,0 +1,33 @@
+class Publisher:
+    users = set()
+
+    def register(self, user):
+        self.users.add(user)
+
+    def unregister(self, user):
+        self.users.discard(user)
+
+    def send_notifications(self, message):
+        for user in self.users:
+            user.notify(message)
+
+
+class Subscriber:
+
+    def __init__(self, username):
+        self.username = username
+
+    def notify(self, message):
+        print(self.username + ' received message: ' + message)
+
+publisher = Publisher()
+
+# Create observers that will receive notifications
+bucky = Subscriber('Bucky')
+sally = Subscriber('Sally')
+publisher.register(bucky)
+publisher.register(sally)
+
+# Notifications are sent to every observer (subscriber)
+publisher.send_notifications('We updated the website')
+publisher.send_notifications('Make sure to add a profile picture')
